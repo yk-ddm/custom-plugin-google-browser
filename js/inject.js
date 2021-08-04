7f9c39938e2f984be9edeb56c7fa7f52b8b97fed
yk-ddm <1715858747@qq.com>
2021-08-04 22:52:52 +0800

    add demoTwo: modified btn text

--- js/inject.js
+++ js/inject.js
@@ -2,20 +2,39 @@
  * @Author: yk 
  * @Date: 2021-08-04 17:40:23 
  * @Last Modified by: yk
- * @Last Modified time: 2021-08-04 21:55:39
+ * @Last Modified time: 2021-08-04 22:49:47
  */
-var oSu = document.getElementById('su'),
-    oKw = document.getElementById('kw'),
-    oInput = document.createElement('input')
 
-oInput.type = 'button'
-oInput.className = 'bg s_btn'
-oInput.id = 'yk'
-oInput.value = 'yk'
-// oInput.setAttribute('value', 'yk')
-oInput.style.marginTop = '15px'
-oInput.onclick = function ()
+/**
+ * @description modified baidu.com home html
+ * 
+ * add input tag to baidu.com html
+ * 
+ */
+function demoOne()
 {
-    oKw.value = 'yk test'
+    var oSu = document.getElementById('su'),
+        oKw = document.getElementById('kw'),
+        oInput = document.createElement('input')
+
+    oInput.type = 'button'
+    oInput.className = 'bg s_btn'
+    oInput.id = 'yk'
+    oInput.value = 'yk'
+    // oInput.setAttribute('value', 'yk')
+    oInput.style.marginTop = '15px'
+    oInput.onclick = function ()
+    {
+        oKw.value = 'yk test'
+    }
+    oSu.parentElement.appendChild(oInput)
 }
-oSu.parentElement.appendChild(oInput)
+
+function demoTwo()
+{
+    document.getElementById('su').setAttribute('value', 'demoTwo test')
+    document.getElementById('kw').setAttribute('value', 'demoTwo')
+    console.log(document.getElementById('su').value)
+}
+
+demoTwo()
