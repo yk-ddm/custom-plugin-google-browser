b493066bb63c86e121a3b5268478a0adf8c669ea
yk-ddm <1715858747@qq.com>
2021-08-04 22:25:49 +0800

    modified baidu html

--- js/inject.js
+++ js/inject.js
@@ -2,7 +2,20 @@
  * @Author: yk 
  * @Date: 2021-08-04 17:40:23 
  * @Last Modified by: yk
- * @Last Modified time: 2021-08-04 17:46:15
+ * @Last Modified time: 2021-08-04 21:55:39
  */
+var oSu = document.getElementById('su'),
+    oKw = document.getElementById('kw'),
+    oInput = document.createElement('input')
 
-alert('test')
+oInput.type = 'button'
+oInput.className = 'bg s_btn'
+oInput.id = 'yk'
+oInput.value = 'yk'
+// oInput.setAttribute('value', 'yk')
+oInput.style.marginTop = '15px'
+oInput.onclick = function ()
+{
+    oKw.value = 'yk test'
+}
+oSu.parentElement.appendChild(oInput)
